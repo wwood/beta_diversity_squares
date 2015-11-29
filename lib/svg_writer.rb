e05422e9ea769da30e555f99ba1015a66c9c9391
Ben Woodcroft <donttrustben@gmail.com>
2015-11-29 12:42:24 +1000

    performance improvements, damn String#+

--- lib/svg_writer.rb
+++ lib/svg_writer.rb
@@ -1,27 +1,27 @@
 #Take some of the hassle out of writing the svgs. Very primitive.
 class SVGWriter
   def initialize(width, height)
-    @svg = "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"#{width}\" height=\"#{height}\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n"
+    @svg = ["<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"#{width}\" height=\"#{height}\" xmlns:xlink=\"http://www.w3.org/1999/xlink\">\n"]
   end
 
   def rect(attributes={})
-    @svg += "<rect"
+    @svg.push "<rect"
     attributes.each do |key, value|
-      @svg += " #{key}=\"#{value}\""
+      @svg.push " #{key}=\"#{value}\""
     end
-    @svg += " />\n"
+    @svg.push " />\n"
   end
   alias_method :rectangle, :rect
 
   def text(words, attributes={})
-    @svg += "<text"
+    @svg.push "<text"
     attributes.each do |key, value|
-      @svg += " #{key}=\"#{value}\""
+      @svg.push " #{key}=\"#{value}\""
     end
-    @svg += ">#{words}</text>"
+    @svg.push ">#{words}</text>"
   end
 
   def svg
-    @svg + "</svg>"
+    [@svg, "</svg>"].join('')
   end
 end
